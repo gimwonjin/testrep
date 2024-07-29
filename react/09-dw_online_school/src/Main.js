64f0bd642f28c134902db7aa0cbb16569d5948ee
LeeJh <ghjkl12345asd@gmail.com>
2024-07-29 12:40:42 +0900

    111

--- react/09-dw_online_school/src/Main.js
+++ react/09-dw_online_school/src/Main.js
@@ -11,7 +11,7 @@ import WishListPage from "./pages/WishListPage";
 
 function Main(props) {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
